15834e83c60c5a01f2a1cb0df7b57376a74d2c1c
kiranmkr <117913744+amantausif2003@users.noreply.github.com>
2022-12-03 22:20:48 +0500

    under_dev

--- app/src/main/java/com/example/designstudio/MainActivity.kt
+++ app/src/main/java/com/example/designstudio/MainActivity.kt
@@ -2,10 +2,13 @@ package com.example.designstudio
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.designstudio.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var mainBinding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mainBinding.root)
     }
 }
