cf0d5dfa3b0d4a2e33213a618551ceabc2f03b05
kiranmkr <117913744+amantausif2003@users.noreply.github.com>
2022-12-04 13:50:08 +0500

    under_dev

--- app/src/main/java/com/example/designstudio/ui/MainActivity.kt
+++ app/src/main/java/com/example/designstudio/ui/MainActivity.kt
@@ -5,30 +5,52 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.util.Log
+import androidx.cardview.widget.CardView
 import androidx.core.content.ContextCompat
 import androidx.transition.TransitionManager
 import com.example.designstudio.R
 import com.example.designstudio.customCallBack.TemplateClickCallBack
 import com.example.designstudio.databinding.ActivityMainBinding
+import com.example.designstudio.databinding.CustomHomeUiBinding
+import com.example.designstudio.recyclerAdapter.MainRecyclerAdapter
 import com.example.designstudio.util.Utils
+import java.util.ArrayList
 
 class MainActivity : AppCompatActivity(), TemplateClickCallBack {
 
     private lateinit var mainBinding: ActivityMainBinding
     private var workerHandler = Handler(Looper.getMainLooper())
+    private lateinit var homeRoot: CustomHomeUiBinding
+    private var cardListView: ArrayList<CardView> = ArrayList()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mainBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(mainBinding.root)
 
+        homeRoot = CustomHomeUiBinding.bind(mainBinding.homeRoot.root)
+
         updateUi()
+
         updateUiClick()
 
     }
 
     private fun updateUi() {
+
         homeSelection()
+
+        cardListView.add(homeRoot.cardSvg)
+        cardListView.add(homeRoot.cardSticker)
+        cardListView.add(homeRoot.cardMono)
+        cardListView.add(homeRoot.cardWaterColor)
+        cardListView.add(homeRoot.cardShape)
+
+        homeRoot.mainRecycler.apply {
+//            setHasFixedSize(true)
+            adapter = MainRecyclerAdapter()
+        }
     }
 
     private fun homeSelection() {
@@ -69,9 +91,7 @@ class MainActivity : AppCompatActivity(), TemplateClickCallBack {
 
     private fun updateUiClick() {
 
-        /*mainBinding.goPro.setOnClickListener {
-            Utils.showToast(this, "calling Go To Pro")
-        }*/
+        homeIconClick()
 
         mainBinding.btnHome.setOnClickListener {
             homeSelection()
@@ -82,14 +102,62 @@ class MainActivity : AppCompatActivity(), TemplateClickCallBack {
         }
 
         mainBinding.btnSetting.setOnClickListener {
-           settingSelection()
+            settingSelection()
+        }
+
+    }
+
+    private fun homeIconClick() {
+
+        homeRoot.goPro.setOnClickListener {
+            Utils.showToast(this, "calling Go to Pro")
+        }
+
+        homeRoot.cardSvg.setOnClickListener {
+            cardSelectionForAll(cardListView,it.id)
+        }
+        homeRoot.cardSticker.setOnClickListener {
+            cardSelectionForAll(cardListView,it.id)
+        }
+        homeRoot.cardMono.setOnClickListener {
+            cardSelectionForAll(cardListView,it.id)
+        }
+        homeRoot.cardWaterColor.setOnClickListener {
+            cardSelectionForAll(cardListView,it.id)
+        }
+        homeRoot.cardShape.setOnClickListener {
+            cardSelectionForAll(cardListView,it.id)
+        }
+    }
+
+    private fun cardSelectionForAll(views: ArrayList<CardView>, view_id: Int) {
+
+        for (i in views.indices) {
+
+            if (views[i].id == view_id) {
+                views[i].setCardBackgroundColor(
+                    ContextCompat.getColor(
+                        this@MainActivity,
+                        R.color.colorAccent
+                    )
+                )
+            } else {
+                Log.d("myCard", "Card is disable ")
+                views[i].setCardBackgroundColor(
+                    ContextCompat.getColor(
+                        this@MainActivity,
+                        R.color.cardBack
+                    )
+                )
+            }
+
+
         }
     }
 
     override fun onItemClickListener(labelStatus: Boolean) {
 
     }
-
 
     private fun showAnimation() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
